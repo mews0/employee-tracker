aea9f9ec6052f4a0cdfe5f991d51ce00dd10b610
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-08-19 12:36:09 -0500

    Create and populate employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,8 +3,26 @@ VALUES ('Operations'), ('HR'), ('Finance'), ('Marketing'), ('IT');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Associate', 50000, 1),
-  ('Associate', 50000, 2),
-  ('Associate', 50000, 3),
-  ('Associate', 50000, 4),
-  ('Associate', 50000, 5);
+  ('Operations Manager', 75000, 1),
+  ('HR Manager', 75000, 2),
+  ('Finance Manager', 75000, 3),
+  ('Marketing Manager', 75000, 4),
+  ('IT Manager', 75000, 5),
+  ('Operations Associate', 50000, 1),
+  ('HR Associate', 50000, 2),
+  ('Finance Associate', 50000, 3),
+  ('Marketing Associate', 50000, 4),
+  ('IT Associate', 50000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Cosgrove', 'Shumway', 1, NULL),
+  ('Doink', 'Ahanahue', 2, NULL),
+  ('Legume', 'Duprix', 3, NULL),
+  ('Grunky', 'Peep', 4, NULL),
+  ('Strunk', 'Flugget', 5, NULL),
+  ('Stumptavian', 'Roboclick', 6, 1),
+  ('Vagonius', 'Thicket-Suede', 7, 2),
+  ('Marmadune', 'Shazbot', 8, 3),
+  ('Faux', 'Doadles', 9, 4),
+  ('Snarf', 'Mintz-Plasse', 10, 5);
