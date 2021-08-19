e2eeee2d65245ae6386c9cee45168ed0d1ba7162
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-08-19 11:02:02 -0500

    Create and populate role table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,15 @@
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
 
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,10 @@
 INSERT INTO department (name)
 VALUES ('Operations'), ('HR'), ('Finance'), ('Marketing'), ('IT');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Associate', 50000, 1),
+  ('Associate', 50000, 2),
+  ('Associate', 50000, 3),
+  ('Associate', 50000, 4),
+  ('Associate', 50000, 5);
