407dfaa9e070b5b6882c31a6de85a5b2792fe332
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-09-16 11:35:03 -0500

    Add choices.js

---
+++ choices.js
@@ -0,0 +1,26 @@
+const userChoice = require('./db/query');
+
+// this file returns an array of choices that will be used in Inquirer
+const choices = {
+  // return array of department names
+  listDepartments() {
+    
+  },
+
+  // return array of role titles
+  listTitles() {
+
+  },
+  
+  // return array of manager names
+  listManagers() {
+
+  },
+  
+  // return array of employee names
+  listEmployees() {
+
+  }
+}
+
+module.exports = choices;
