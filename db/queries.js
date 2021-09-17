f45d3d301adc4943b179155af7bf3759608313bc
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-09-17 14:14:56 -0500

    Update queries.js

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const db = require('./connection');
 const cTable = require('console.table');
 
 const queries = {
-  // return all records indepartment table
+  // return all records from department table
   findAllDepartments() {
     const sql = `SELECT * FROM department`;
     db.query(sql, (err, rows) => {
@@ -31,7 +31,7 @@ const queries = {
     return list;
   },
 
-  // return all records in role table
+  // return all records from role table
   findAllRoles() {
     const sql = `SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON role.department_id = department.id`;
     db.query(sql, (err, rows) => {
@@ -60,6 +60,7 @@ const queries = {
     return list;
   },
 
+  // return all records from employee table
   findAllEmployees() {
     const sql = `SELECT emp.id, emp.first_name, emp.last_name, role.title, department.name AS department, role.salary, CONCAT(mgr.first_name, ' ', mgr.last_name) AS manager FROM employee emp LEFT JOIN employee mgr ON emp.manager_id = mgr.id LEFT JOIN role ON emp.role_id = role.id LEFT JOIN department ON role.department_id = department.id`
     db.query(sql, (err, rows) => {
@@ -87,15 +88,28 @@ const queries = {
     });
     return list;
   },
-  
+
   // return array of employee names
   listEmployees() {
     const list = [];
-    const sql =`SELECT CONCAT(first_name, ' ', last_name) AS name FROM employee`;
-    
+    const sql = `SELECT CONCAT(first_name, ' ', last_name) AS name FROM employee`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        console.error('Error.');
+        return;
+      }
+      const arr = JSON.parse(JSON.stringify(rows));
+      for (let i = 0; i < arr.length; i++) {
+        list.push(arr[i].name);
+      }
+      console.log(list); // *** LIST IS DISPLAYING AS INTENDED HERE ***
+    });
+    console.log(list); /* LIST IS NOT DISPLAYING AS INTENDED HERE, AND I AM STRUGGLING TO FIGURE OUT WHY.
+    LISTMANAGERS() WORKS, AND IT IS IDENTICAL TO LISTEMPLOYEES() EXCEPT FOR THE VALUE IN SQL. */ 
     return list;
   },
 
+  // create new record in department table
   createDepartment(name) {
     const sql = `INSERT INTO department (name) VALUES (?)`;
     const params = name;
@@ -108,6 +122,7 @@ const queries = {
     });
   },
 
+  // create new record in role table
   createRole(title, salary, department_id) {
     const sql = `INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`;
     const params = [title, salary, department_id];
@@ -120,6 +135,7 @@ const queries = {
     });
   },
 
+  // create new record in employee table
   createEmployee(first_name, last_name, role_id, manager_id) {
     const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
     const params = [first_name, last_name, role_id, manager_id]
@@ -132,6 +148,7 @@ const queries = {
     });
   },
 
+  // update a role in employee table
   updateEmployeeRole(role_id, id) {
     const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
     const params = [role_id, id]
